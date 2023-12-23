a58df31ff9014cd08a83a0e1a907606a10ffb256
Joynal Abedin <44470728+Joynal279@users.noreply.github.com>
2023-12-23 15:02:02 +0600

    Initial commit

---
+++ ToastAlertExample/ContentView.swift
@@ -0,0 +1,62 @@
+//
+//  ContentView.swift
+//  ToastAlertExample
+//
+//  Created by Joynal Abedin on 23/12/23.
+//
+
+import SwiftUI
+import ToastAlertSwiftPackage
+
+//MARK: - ContentView
+struct ContentView: View {
+    
+    /// `Properties`
+    @State private var toast: ToastView? = nil
+    @State private var presentAlert: Bool = false
+    
+    var body: some View {
+        ZStack {
+            VStack {
+                
+                Button("Show Alert") { /// `Alert button`
+                    presentAlert = true
+                }
+                .buttonStyle(.borderedProminent)
+                
+                Button("Show Toast") { /// `Toast button`
+                    toast = ToastView(type: .success, title: "Success", message: "This is success message", duration: 3.0)
+                    //toast = CustomToast(type: .error, title: "Error", message: "This is error message", duration: 5.0)
+                    //toast = CustomToast(type: .info, title: "Info", message: "This is info message", duration: 3.0)
+                    //toast = CustomToast(type: .warning, title: "Warning", message: "This is warning message")
+                }
+                .buttonStyle(.borderedProminent)
+                
+            }//: end VStack
+            .padding()
+            
+            /// `Present Alert`
+            if presentAlert{
+                //CustomAlert(presentAlert: $presentAlert, alertType: .constant(.oneButton(title: "Do you want to delete?", message: "If you delete this file then you won’t please again check everything"))){ withAnimation{  presentAlert.toggle() }
+                //} rightButtonAction: { withAnimation{ presentAlert.toggle() } }
+                
+                CustomAlert(presentAlert: $presentAlert, alertType: .constant(.twoButton(title: "Do you want to delete?", message: "If you delete this file then you won’t please again check everything"))){
+                    withAnimation{
+                        presentAlert.toggle()
+                    }
+                } rightButtonAction: {
+                    withAnimation{
+                        presentAlert.toggle()
+                    }
+                }
+            }//: End present alert
+        }
+        .toastView(toast: $toast)
+        
+    }
+}
+
+//MARK: - Preview
+#Preview {
+    ContentView()
+}
---
+++ ToastAlertExample/ToastAlertExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToastAlertExampleApp.swift
+//  ToastAlertExample
+//
+//  Created by Joynal Abedin on 23/12/23.
+//
+
+import SwiftUI
+
+@main
+struct ToastAlertExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
